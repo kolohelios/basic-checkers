33116f05744973fe8d75d1613f84fa71309dd2c6
Jon Edwards <jkedwards@me.com>
2015-04-10 11:23:47 -0700

    add images and classes

--- client/index.js
+++ client/index.js
@@ -7,5 +7,7 @@ function init(){
 }
 
 function initBoard(){
-  
+  $('#board tr:lt(3) .valid').addClass('p1 pig');
+  $('#board tr:gt(4) .valid').addClass('p2 bird');
+  $('#board td:not(.p1 .p2)').addClass('empty');
 }
